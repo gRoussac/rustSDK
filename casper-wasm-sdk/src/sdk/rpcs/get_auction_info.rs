5ff150b18ec2f6d3e10245fef025ecbc88a12716
gRoussac <gRoussac@users.noreply.github.com>
2023-08-21 19:34:23 +0200

    get auction api

--- casper-wasm-sdk/src/sdk/rpcs/get_auction_info.rs
+++ casper-wasm-sdk/src/sdk/rpcs/get_auction_info.rs
@@ -2,28 +2,63 @@
 use crate::helpers::serialize_result;
 use crate::{
     helpers::get_verbosity_or_default,
-    types::{block_identifier::BlockIdentifier, verbosity::Verbosity},
+    types::{block_identifier::BlockIdentifier, sdk_error::SdkError, verbosity::Verbosity},
     SDK,
 };
 use casper_client::{
-    get_auction_info, rpcs::results::GetAuctionInfoResult, Error, JsonRpcId, SuccessResponse,
+    cli::get_auction_info as get_auction_info_cli, get_auction_info as get_auction_info_lib,
+    rpcs::results::GetAuctionInfoResult, JsonRpcId, SuccessResponse,
 };
+#[cfg(target_arch = "wasm32")]
+use gloo_utils::format::JsValueSerdeExt;
 use rand::Rng;
 #[cfg(target_arch = "wasm32")]
+use serde::Deserialize;
+#[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
+
+#[derive(Debug, Deserialize, Clone, Default)]
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+pub struct GetAuctionInfoOptions {
+    node_address: String,
+    maybe_block_id: Option<String>,
+    maybe_block_identifier: Option<BlockIdentifier>,
+    verbosity: Option<Verbosity>,
+}
+
+pub enum BlockIdentifierInput {
+    BlockIdentifier(BlockIdentifier),
+    Id(String),
+}
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen]
 impl SDK {
+    #[wasm_bindgen(js_name = "get_auction_info_options")]
+    pub fn get_auction_info_options(&self, options: JsValue) -> GetAuctionInfoOptions {
+        options.into_serde().unwrap_or_default()
+    }
+
     #[wasm_bindgen(js_name = "get_auction_info")]
-    pub async fn get_auction_info_js_alias(
-        &mut self,
-        node_address: &str,
-        verbosity: Option<Verbosity>,
-        maybe_block_identifier: Option<BlockIdentifier>,
-    ) -> JsValue {
+    pub async fn get_auction_info_js_alias(&mut self, options: GetAuctionInfoOptions) -> JsValue {
+        let GetAuctionInfoOptions {
+            node_address,
+            maybe_block_id,
+            maybe_block_identifier,
+            verbosity,
+        } = options;
+
+        let maybe_block_identifier = if let Some(maybe_block_identifier) = maybe_block_identifier {
+            Some(BlockIdentifierInput::BlockIdentifier(
+                maybe_block_identifier,
+            ))
+        } else {
+            maybe_block_id.map(BlockIdentifierInput::Id)
+        };
+
         serialize_result(
-            self.get_auction_info(node_address, verbosity, maybe_block_identifier)
+            self.get_auction_info(&node_address, maybe_block_identifier, verbosity)
                 .await,
         )
     }
@@ -33,16 +68,37 @@ impl SDK {
     pub async fn get_auction_info(
         &mut self,
         node_address: &str,
+        maybe_block_identifier: Option<BlockIdentifierInput>,
         verbosity: Option<Verbosity>,
-        maybe_block_identifier: Option<BlockIdentifier>,
-    ) -> Result<SuccessResponse<GetAuctionInfoResult>, Error> {
+    ) -> Result<SuccessResponse<GetAuctionInfoResult>, SdkError> {
         //log("get_auction_info!");
-        get_auction_info(
-            JsonRpcId::from(rand::thread_rng().gen::<i64>().to_string()),
-            node_address,
-            get_verbosity_or_default(verbosity).into(),
-            maybe_block_identifier.map(Into::into),
-        )
-        .await
+
+        if let Some(BlockIdentifierInput::Id(maybe_block_id)) = maybe_block_identifier {
+            get_auction_info_cli(
+                &rand::thread_rng().gen::<i64>().to_string(),
+                node_address,
+                get_verbosity_or_default(verbosity).into(),
+                &maybe_block_id,
+            )
+            .await
+            .map_err(SdkError::from)
+        } else {
+            let maybe_block_identifier =
+                if let Some(BlockIdentifierInput::BlockIdentifier(maybe_block_identifier)) =
+                    maybe_block_identifier
+                {
+                    Some(maybe_block_identifier)
+                } else {
+                    None
+                };
+            get_auction_info_lib(
+                JsonRpcId::from(rand::thread_rng().gen::<i64>().to_string()),
+                node_address,
+                get_verbosity_or_default(verbosity).into(),
+                maybe_block_identifier.map(Into::into),
+            )
+            .await
+            .map_err(SdkError::from)
+        }
     }
 }
