b56f28ad0a5ab9bacea429b7f5706a8189810911
gRoussac <gRoussac@users.noreply.github.com>
2023-09-01 02:31:31 +0200

    update motesToCSPR

--- casper-wasm-sdk/src/js/interns.rs
+++ casper-wasm-sdk/src/js/interns.rs
@@ -1,8 +1,11 @@
+use crate::debug::log;
 use crate::helpers::get_current_timestamp;
 use crate::helpers::hex_to_uint8_vec;
 use crate::{debug::error, helpers::secret_key_from_pem, types::verbosity::Verbosity};
 use casper_types::PublicKey;
 use gloo_utils::format::JsValueSerdeExt;
+use rust_decimal::prelude::*;
+use std::str::FromStr;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(js_name = "hexToString")]
@@ -16,6 +19,26 @@ pub fn hex_to_string_js_alias(hex_string: &str) -> String {
 #[wasm_bindgen(js_name = "hexToUint8Array")]
 pub fn hex_to_uint8_vec_js_alias(hex_string: &str) -> Vec<u8> {
     hex_to_uint8_vec(hex_string)
+}
+
+#[wasm_bindgen(js_name = "motesToCSPR")]
+pub fn motes_to_cspr_js_alias(motes: &str) -> String {
+    match Decimal::from_str(motes) {
+        Ok(motes_decimal) => {
+            let cspr_decimal = motes_decimal / Decimal::new(1_000_000_000, 0);
+            let cspr_f64 = cspr_decimal.to_f64().unwrap_or(0.0);
+            let formatted_cspr = format!("{:.2}", cspr_f64);
+            if formatted_cspr.ends_with(".00") {
+                formatted_cspr.replace(".00", "")
+            } else {
+                formatted_cspr
+            }
+        }
+        Err(_) => {
+            error("Failed to parse input as Decimal");
+            "Invalid input".to_string()
+        }
+    }
 }
 
 #[wasm_bindgen(js_name = "jsonPrettyPrint")]
