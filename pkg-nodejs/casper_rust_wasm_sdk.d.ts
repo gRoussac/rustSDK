f4e70976fb616e14917cdb82e3940c7c7edcf21b
gRoussac <gRoussac@users.noreply.github.com>
2024-06-13 12:14:36 +0200

    Try doc action

--- pkg-nodejs/casper_rust_wasm_sdk.d.ts
+++ pkg-nodejs/casper_rust_wasm_sdk.d.ts
@@ -176,6 +176,13 @@ export function encodeLowerBlake2b(meta_data: string): any;
 export function makeDictionaryItemKey(key: Key, value: string): string;
 /**
 */
+export enum Verbosity {
+  Low = 0,
+  Medium = 1,
+  High = 2,
+}
+/**
+*/
 export enum TransactionKind {
   InvocableEntity = 0,
   InvocableEntityAlias = 1,
@@ -188,13 +195,6 @@ export enum TransactionKind {
   Undelegate = 8,
   Redelegate = 9,
   WithdrawBid = 10,
-}
-/**
-*/
-export enum Verbosity {
-  Low = 0,
-  Medium = 1,
-  High = 2,
 }
 /**
 */
