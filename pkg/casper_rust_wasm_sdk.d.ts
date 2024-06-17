fadc186b8da0643f294ca4dcc0cfcd136edb4a36
gRoussac <gRoussac@users.noreply.github.com>
2024-06-17 23:55:41 +0200

    Update packages

--- pkg/casper_rust_wasm_sdk.d.ts
+++ pkg/casper_rust_wasm_sdk.d.ts
@@ -171,17 +171,17 @@ export function encodeLowerBlake2b(meta_data: string): any;
 export function makeDictionaryItemKey(key: Key, value: string): string;
 /**
 */
-export enum Verbosity {
-  Low = 0,
-  Medium = 1,
-  High = 2,
-}
-/**
-*/
 export enum TransferTargetKind {
   PublicKey = 0,
   AccountHash = 1,
   URef = 2,
+}
+/**
+*/
+export enum Verbosity {
+  Low = 0,
+  Medium = 1,
+  High = 2,
 }
 /**
 */
