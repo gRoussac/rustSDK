4e99ed33c8d9561bddc6ee51c03e668603781336
gRoussac <gRoussac@users.noreply.github.com>
2024-08-08 12:53:11 +0200

    Update build

--- pkg/casper_rust_wasm_sdk.d.ts
+++ pkg/casper_rust_wasm_sdk.d.ts
@@ -171,6 +171,13 @@ export function encodeLowerBlake2b(meta_data: string): any;
 export function makeDictionaryItemKey(key: Key, value: string): string;
 /**
 */
+export enum PricingMode {
+  Fixed = 0,
+  Classic = 1,
+  Reserved = 2,
+}
+/**
+*/
 export enum TransactionKind {
   InvocableEntity = 0,
   InvocableEntityAlias = 1,
@@ -186,13 +193,6 @@ export enum TransactionKind {
 }
 /**
 */
-export enum TransferTargetKind {
-  PublicKey = 0,
-  AccountHash = 1,
-  URef = 2,
-}
-/**
-*/
 export enum Verbosity {
   Low = 0,
   Medium = 1,
@@ -200,10 +200,10 @@ export enum Verbosity {
 }
 /**
 */
-export enum PricingMode {
-  Fixed = 0,
-  Classic = 1,
-  Reserved = 2,
+export enum TransferTargetKind {
+  PublicKey = 0,
+  AccountHash = 1,
+  URef = 2,
 }
 /**
 */
