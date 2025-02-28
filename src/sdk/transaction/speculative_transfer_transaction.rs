eee40b611d16f4411b28c7208cc51de341aec778
gRoussac <gRoussac@users.noreply.github.com>
2025-02-28 22:58:15 +0100

    Test failing

--- src/sdk/transaction/speculative_transfer_transaction.rs
+++ src/sdk/transaction/speculative_transfer_transaction.rs
@@ -213,6 +213,7 @@ mod tests {
         assert!(result.is_err());
 
         let err_string = result.err().unwrap().to_string();
+        dbg!(err_string);
         assert!(err_string.contains(error_message));
     }
 }
