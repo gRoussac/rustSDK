82bb78bcc9dfdd19aadd62a5f399f69bbf0aa28e
gRoussac <gRoussac@users.noreply.github.com>
2025-06-04 23:46:45 +0200

    Test e2e

--- tests/e2e/jest.config.ts
+++ tests/e2e/jest.config.ts
@@ -6,7 +6,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  testTimeout: 40000,
+  testTimeout: 60000,
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
