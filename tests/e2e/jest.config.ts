6a996c7a1b13063a464a0248273ddcefe7f3e0d0
gRoussac <gRoussac@users.noreply.github.com>
2025-06-04 22:46:40 +0200

    Increase timeout e2e

--- tests/e2e/jest.config.ts
+++ tests/e2e/jest.config.ts
@@ -6,7 +6,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  testTimeout: 20000,
+  testTimeout: 40000,
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
